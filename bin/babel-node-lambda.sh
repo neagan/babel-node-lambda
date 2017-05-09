004943feb2cbd561a9f918d50c3527d8ddc55394
Nick Eagan <nick.eagan@timeinc.com>
2017-05-09 00:32:03 -0700

    use babel-register to compile handler imports

--- bin/babel-node-lambda.sh
+++ bin/babel-node-lambda.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
 if [ ! -f .babelrc ]; then
-  cp node_modules/babel-node-lambda/.babelrc .
+  echo No .babelrc found
+  exit
 fi
 
-if [ $# -lt 2 ]; then
-  babel index.js --out-file index-compiled.js
-  node-lambda run --handler index-compiled.handler
+case $1 in
+  run)
+    case $2 in
+      -H|--handler)
+        IFS=' . ' read -r -a HANDLER <<< $3
 
-  rm index-compiled.js
-else
-  case $1 in
-    run)
-      case $2 in
-        -H|--handler)
-          IFS=' . ' read -r -a HANDLER <<< $3
-
-          babel ${HANDLER[0]}.js --out-file ${HANDLER[0]}-compiled.js
-          node-lambda run --handler ${HANDLER[0]}-compiled.${HANDLER[1]}
-
-          rm ${HANDLER[0]}-compiled.js
-          ;;
-        *)
-          echo Flag \"$2\" is not currently supported
-      esac
-      ;;
-    *)
-      echo Command \"$1\" is not currently supported
-  esac
-
-
-fi
+        echo "require('babel-register');exports.${HANDLER[1]} = require(\"./${HANDLER[0]}\").${HANDLER[1]};" > index-compiled.js
+        node-lambda run --handler index-compiled.${HANDLER[1]}
+        rm index-compiled.js
+        ;;
+      "")
+        echo "require('babel-register');exports.handler = require('./index').handler;" > index-compiled.js
+        node-lambda run --handler index-compiled.handler
+        rm index-compiled.js
+        ;;
+      *)
+        echo Flag \"$2\" is not currently supported
+    esac
+    ;;
+  *)
+    echo Command \"$1\" is not currently supported
+esac
